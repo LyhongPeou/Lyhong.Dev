8cfb390b1ae11c68eb66e27b4a1bd53c2337cf24
peoul <peoul@oregonstate.edu>
2023-05-15 18:11:12 -0700

    Added Animation Scroll

--- FrontEnd/src/Pages/MainPage.jsx
+++ FrontEnd/src/Pages/MainPage.jsx
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from "react";
 
 import WelcomeSection from "../Components/WelcomeSection";
 import ClassesSection from "../Components/ClassesSection";
+import { ScrollContainer } from "react-scroll-motion";
 
 const MainPage = () => {
   useEffect(() => {
@@ -11,10 +12,10 @@ const MainPage = () => {
 
 
   return (
-    <Fragment>
-    <WelcomeSection/>
-    <ClassesSection/>
-    </Fragment>
+    <ScrollContainer>
+   <WelcomeSection/>
+   <ClassesSection/>
+    </ScrollContainer>
   
   );
 };
