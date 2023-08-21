2fdd76c4c839f00dea3b38c6491368648e4cc125
peoul <peoul@oregonstate.edu>
2023-08-20 23:05:41 -0700

    setting up for props

--- FrontEnd/src/Components/Section/Skill/SkillBox.jsx
+++ FrontEnd/src/Components/Section/Skill/SkillBox.jsx
@@ -1,15 +1,32 @@
 import Card from "react-bootstrap/Card";
 import Classes from "./SkillBox.module.css";
+import { useState } from "react";
 
 const SkillBox = (props) => {
+
+  const [show, setShow] = useState(false);
+
+ const handleShow = () =>{
+
+  if (show == true){
+    setShow(false)
+  } else {
+    setShow(true)
+  }
+
+  console.log(props)
+
+ }
+
+
   const { skills } = props;
 
   return (
     <div className={Classes.SkillList}>
       {skills.map((skill) => {
         return (
-          <div className="d-flex flex-row text-center">
-            <Card style={{ color: "black" }} className="bg-dark  text-white" P>
+          <div className="d-flex flex-row text-center" onClick={handleShow}>
+            <Card style={{ color: "black" }} className="bg-dark  text-white" >
               <Card.Img
                 variant="top"
                 src={skill.imageURL}
